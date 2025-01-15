c2669b5e37fb76163e744178593cdac87bb2083a
Euiseo Cha <zeroday0619_dev@outlook.com>
2025-01-15 17:54:51 +0900

    feat: cloudflare page 대응

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -8,6 +9,10 @@ const __dirname = dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
+
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
